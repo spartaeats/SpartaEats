fbee1a316d29fd5411b2873f986433bedff547a6
rudcjs <rudcjs335@gmail.com>
2025-09-30 15:08:29 +0900

    init commit

---
+++ src/main/java/com/sparta/sparta_eats/SpartaEatsApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.sparta_eats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaEatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaEatsApplication.class, args);
+    }
+
+}
