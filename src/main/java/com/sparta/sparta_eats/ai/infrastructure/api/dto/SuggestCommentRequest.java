9924e4f39ebd7c7aae23b9ca49ca15f2718c0649
rudcjs <rudcjs335@gmail.com>
2025-10-12 15:11:24 +0900

    refactor: 테스트 코드 개선

--- src/main/java/com/sparta/sparta_eats/ai/infrastructure/api/dto/SuggestCommentRequest.java
+++ src/main/java/com/sparta/sparta_eats/ai/infrastructure/api/dto/SuggestCommentRequest.java
@@ -1,7 +1,10 @@
 package com.sparta.sparta_eats.ai.infrastructure.api.dto;
 
+import lombok.Builder;
+
 import java.util.List;
 
+@Builder
 public record SuggestCommentRequest(String storeName,
                                     String nickname,
                                     Integer rate,
--- src/main/java/com/sparta/sparta_eats/ai/infrastructure/api/dto/SuggestItemDescriptionRequest.java
+++ src/main/java/com/sparta/sparta_eats/ai/infrastructure/api/dto/SuggestItemDescriptionRequest.java
@@ -1,5 +1,8 @@
 package com.sparta.sparta_eats.ai.infrastructure.api.dto;
 
+import lombok.Builder;
+
+@Builder
 public record SuggestItemDescriptionRequest(String name,
                                             String simpleDescription,
                                             String feature) {
