6f407a89bc2620ca4b6d69de2ee2cf7842bfe239
rudcjs <rudcjs335@gmail.com>
2025-10-16 10:46:27 +0900

    feat: createOrder - 테스트 실행 전 - controller 엔드 포인트 생성

--- src/main/java/com/sparta/sparta_eats/order/presentation/OrderController.java
+++ src/main/java/com/sparta/sparta_eats/order/presentation/OrderController.java
@@ -1,4 +1,30 @@
 package com.sparta.sparta_eats.order.presentation;
 
+import com.sparta.sparta_eats.global.infrastructure.config.security.UserDetailsImpl;
+import com.sparta.sparta_eats.order.application.service.OrderService;
+import com.sparta.sparta_eats.order.presentation.dto.request.OrderCreateRequest;
+import com.sparta.sparta_eats.order.presentation.dto.response.OrderResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/orders")
 public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<OrderResponse> createOrder(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                     @RequestBody OrderCreateRequest request) {
+
+        return ResponseEntity.created(URI.create("temp"))
+                .body(orderService.createOrder(userDetails.getUser(), request));
+    }
 }
