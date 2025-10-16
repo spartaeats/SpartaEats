525025c2117dba7f23a4df23ae78e7382bc3d6bb
Robbie <Robbie@email.com>
2025-10-16 12:59:08 +0900

    fix: domain추가로 인한 오류 수정

--- src/main/java/com/sparta/sparta_eats/store/domain/entity/Store.java
+++ src/main/java/com/sparta/sparta_eats/store/domain/entity/Store.java
@@ -19,16 +19,16 @@ public class Store {
     @Column(name = "id", columnDefinition = "UUID")
     private String id;
 
-    @Column(name = "name", nullable = false, length = 255)
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "address", nullable = false, length = 255)
+    @Column(name = "address", nullable = false)
     private String address;
 
-    @Column(name = "addr_detail", length = 255)
+    @Column(name = "addr_detail")
     private String addressDetail;
 
-    @Column(name = "image", length = 255)
+    @Column(name = "image")
     private String image;
 
     @Column(name = "phone", nullable = false, length = 13)
