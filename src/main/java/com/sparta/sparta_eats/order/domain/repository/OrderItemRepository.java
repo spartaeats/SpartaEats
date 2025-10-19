61fafb0df5bf50a4a9cfdf7a41aa36fa95292c47
rudcjs <rudcjs335@gmail.com>
2025-10-19 19:00:05 +0900

    fix: 조회 시 N + 1 문제 발생문제 해결 - OrderItemRepository에 findAllByOrderWithItem 쿼리 작성 - @Transactional(readOnly = true) 추가

--- src/main/java/com/sparta/sparta_eats/order/domain/repository/OrderItemRepository.java
+++ src/main/java/com/sparta/sparta_eats/order/domain/repository/OrderItemRepository.java
@@ -3,6 +3,8 @@ package com.sparta.sparta_eats.order.domain.repository;
 import com.sparta.sparta_eats.order.domain.entity.Order;
 import com.sparta.sparta_eats.order.domain.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +13,7 @@ import java.util.UUID;
 public interface OrderItemRepository extends JpaRepository<OrderItem, UUID> {
     List<OrderItem> findAllByOrder(Order order);
     List<OrderItem> findAllByOrderIn(List<Order> orderList);
+
+    @Query("SELECT oi FROM OrderItem oi JOIN FETCH oi.item WHERE oi.order = :order")
+    List<OrderItem> findAllByOrderWithItem(@Param("order") Order order);
 }
--- src/main/java/com/sparta/sparta_eats/order/domain/repository/OrderRepository.java
+++ src/main/java/com/sparta/sparta_eats/order/domain/repository/OrderRepository.java
@@ -1,10 +1,14 @@
 package com.sparta.sparta_eats.order.domain.repository;
 
 import com.sparta.sparta_eats.order.domain.entity.Order;
+import com.sparta.sparta_eats.order.domain.entity.OrderItem;
 import com.sparta.sparta_eats.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID>, OrderSearchRepository {
